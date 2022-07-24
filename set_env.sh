b6b102f2770e96b0a002fc41a833ff33a063dbb8
Luong Trung <trunghlbuh@gmail.com>
2022-07-24 13:45:36 +0700

    [AWS] init souce

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=trunghl1
+export POSTGRES_PASSWORD=password123
+export POSTGRES_HOST=crd.ccosky68pnis.us-west-2.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=bucket-796706398716-dev
+export AWS_REGION=us-west-2
+export AWS_PROFILE=default
+export JWT_SECRET=hello
+export URL=http://localhost:8100
